1c892adde23b3b8f39f18603208a62d42a4c68b6
Steve <steve@dirtyhooligans.com>
2013-03-27 23:00:03 -0700

    updating the score/time alternates

--- app/ui/public/js/schedule.js
+++ app/ui/public/js/schedule.js
@@ -11,11 +11,47 @@ var initSchedule = function() {
     scheduleData = pageData.data.schedule;
 
     var todaysGame = scheduleData.day_of || {};
-    var nextGame = scheduleData.day_after || scheduleData.next_game;
+    var nextGame = scheduleData.next_game || {};
 
     $("#nextgame_time h4").html(moment(nextGame.start).add('hours', timezoneOffset).format('LT'));
 
     $("#dayof-gametime h3").html(moment(todaysGame.start).add('hours', timezoneOffset).format('LT'));
+
+    if ( todaysGame.scores )
+    {
+        var gametime_title,
+            gametime
+            ;
+        if ( todaysGame.teams.home == 'San Francisco' )
+        {
+            if ( todaysGame.scores.home > todaysGame.scores.away )
+            {
+                gametime_title = 'yay.. they';
+                gametime = 'WON';
+            }
+            else
+            {
+                gametime_title = 'oh darn, they';
+                gametime = 'LOST';
+            }
+        }
+        else
+        {
+            if ( todaysGame.scores.home < todaysGame.scores.away )
+            {
+                gametime_title = 'yay.. they';
+                gametime = 'WON';
+            }
+            else
+            {
+                gametime_title = 'oh darn, they';
+                gametime = 'LOST';
+            }
+        }
+        
+        $("#dayof-gametime-title").html(gametime_title);
+        $("#dayof-gametime h3").html(gametime);
+    }
 }
 
 initSchedule();
